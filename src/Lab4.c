2ce15ee3c2c6d1c557f159633b22b1038c727e31
Rapzy <andrushko.egor@gmail.com>
2019-04-22 15:13:15 +0300

    Add function for reading pid from file

--- src/Lab4.c
+++ src/Lab4.c
@@ -3,6 +3,7 @@
 #include "signal.h"
 #include "string.h"
 #include "unistd.h"
+#include "libgen.h"
  
 //1->2; 2->(3,4); 4->5; 3->6; 6->7; 7->8;
 //1->(8,7) SIGUSR1; 8->(6,5) SIGUSR1; 5->(4,3,2) SIGUSR2; 2->1 SIGUSR2;
@@ -10,9 +11,12 @@ void waitChilds();
 void createFile(int);
 void deleteFiles();
 int checkTree();
+int readPid(int);
 
-int main() 
+char *progname;
+int main(int argc, char *argv[]) 
 {
+    progname = basename(argv[0]);
     signal(SIGINT, deleteFiles);
     printf("Process N: %d | pid: %d\n",0,getpid());
     pid_t pid = fork();
@@ -79,6 +83,7 @@ int main()
         else
         {
             while(!checkTree());
+            printf("%d\n",readPid(8));
             waitChilds();
         }
         //for(;;);
@@ -101,6 +106,23 @@ int checkTree()
             return 0;
     }
     return 1;
+}
+int readPid(int num)
+{
+    char path[30];
+    char pid[10];
+    FILE *f;
+    snprintf(path, sizeof(path), "/tmp/Lab4/%d.txt", num);
+    if((f = fopen(path, "r")) == NULL)
+    {
+        fprintf(stderr, "%s: Couldn't open file %s.\n", progname, path);
+		exit(1);
+    }
+    else
+    {
+        fgets(pid, sizeof(pid), f);
+        return atoi(pid);
+    }
 }
 
 void waitChilds()
